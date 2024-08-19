27e24e5fababb61a928616711aa8442005c1d57a
Breno Leitao <leitao@debian.org>
2024-08-19 03:57:55 -0700

    Add process.py

---
+++ process.py
@@ -0,0 +1,77 @@
+#!/usr/local/bin/python3
+
+import os
+import sys
+import csv
+
+
+
+mitigations= [
+    "CONFIG_MITIGATION_PAGE_TABLE_ISOLATION",
+    "CONFIG_MITIGATION_RETPOLINE",
+    "CONFIG_MITIGATION_RETHUNK",
+    "CONFIG_MITIGATION_UNRET_ENTRY",
+    "CONFIG_MITIGATION_CALL_DEPTH_TRACKING",
+    "CONFIG_MITIGATION_IBPB_ENTRY",
+    "CONFIG_MITIGATION_IBRS_ENTRY",
+    "CONFIG_MITIGATION_SRSO",
+    "CONFIG_MITIGATION_SLS",
+    "CONFIG_MITIGATION_GDS",
+    "CONFIG_MITIGATION_RFDS",
+    "CONFIG_MITIGATION_SPECTRE_BHI",
+    "CONFIG_MITIGATION_MDS",
+    "CONFIG_MITIGATION_TAA",
+    "CONFIG_MITIGATION_MMIO_STALE_DATA",
+    "CONFIG_MITIGATION_L1TF",
+    "CONFIG_MITIGATION_RETBLEED",
+    "CONFIG_MITIGATION_SPECTRE_V1",
+    "CONFIG_MITIGATION_SPECTRE_V2",
+    "CONFIG_MITIGATION_SRBDS",
+    "CONFIG_MITIGATION_SSB"
+]
+
+def parse_output():
+    ret = []
+    entry = read_config()
+    with open("results.log", 'r') as f:
+        for line in f.readlines():
+            if line.startswith("real"):
+                continue
+            elif line.startswith("bench_"):
+                print(line)
+                entry["bench"] = line.strip()
+            elif line.startswith("average"):
+                slices = line.split()
+                entry["real"] = float(slices[2])
+                entry['user'] = float(slices[4])
+                entry['system'] = float(slices[6])
+
+                ret.append(entry)
+                entry = read_config()
+
+    return ret
+
+
+def read_config():
+    entry = {}
+    with open("/tmp/config", 'r') as f:
+        for mitigation in mitigations:
+            entry[mitigation] = 'n'
+            for line in f.readlines():
+                if f"{mitigation}=y" in line:
+                    entry[mitigation] = 'y'
+    return entry
+
+
+# starts here
+os.system("zcat /proc/config.gz | grep CONFIG_MITIGATION > /tmp/config")
+entries = parse_output()
+for data in entries:
+    for k in data.keys():
+        print(f"{k},", end="")
+
+    print()
+    for k in data.keys():
+        print(f"{data[k]},", end="")
+
+    print("\n")
